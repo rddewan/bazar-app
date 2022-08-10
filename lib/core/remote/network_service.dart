e7e57b8a0f456fac3d5c89a2378fe63650ac504e
richard <richard@maqe.com>
2022-08-10 15:18:00 +0700

    added proxy setup

--- lib/core/remote/network_service.dart
+++ lib/core/remote/network_service.dart
@@ -1,6 +1,9 @@
 // Provide a instance od Dio
 
+import 'dart:io';
+
 import 'package:bazar/core/remote/network_service_interceptor.dart';
+import 'package:dio/adapter.dart';
 import 'package:dio/dio.dart';
 import 'package:dio_http_formatter/dio_http_formatter.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -22,6 +25,20 @@ final networkServiceProvider = Provider<Dio>((ref) {
       ref.watch(networkServiceInterceptorProvider)
     ]);
     
+  (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (client) { 
+      // Hook into the findProxy callback to set the client's proxy.
+      client.findProxy = (url) {
+        // replace you proxy ip and port here
+        return 'PROXY 192.168.1.10:9090';
+      };
+      
+      // This is a workaround to allow Proxyman to receive
+      // SSL payloads when your app is running on Android.
+      client.badCertificateCallback = (X509Certificate cert, String host, int port) => Platform.isAndroid;
+
+      return client;
+      
+    }; 
   
   return dio;
 
